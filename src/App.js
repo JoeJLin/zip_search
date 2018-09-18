13d66a939d22982223a979e6035a057f34bb9c34
JoeJLin <Joe.j.lin21@gmail.com>
2018-09-18 14:13:53 -0400

    add api call

--- src/App.js
+++ src/App.js
@@ -2,17 +2,55 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const apiUrl = "http://ctp-zip-api.herokuapp.com/zip/";
+
+function City(props){
+  return (
+    <div></div>
+  );
+}
+
+function ZipSearchFeild(props){
+
+  return (
+    <div>
+      <input type="text" placeHolder="Enter zip code" />
+    </div>
+  );
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cities: [],
+      zipCode: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch(apiUrl + "10002")
+      .then(response => response.json())
+      .then(data => this.setState({cities: data}))
+  }
+
+  updateZip(event){
+    this.setState({zipCode: event.target.value})
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-header">
+          <h2>Zip Code Search</h2>
+        </div>
+        <ZipSearchFeild zipCode={this.state.zipCode} onChange={this.updateZip} />
+        <ul>
+          {this.state.zipCode}
+          {this.state.cities.map(city => <li>{city.RecordNumber}</li>)}
+        </ul>
       </div>
     );
   }
